b8f5d280a8cf46a1ffecb25d1e02ee0f8b7a63e4
江鑫 <jx3313@163.com>
2026-06-24 09:24:05 +0800

    更新main.rs文件，添加文件头注释以包含作者、日期和公司信息

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
+/*
+ * @Author: jiangxin
+ * @Date: 2026-04-15 13:46:55
+ * @Company: orientsec.com.cn
+ * @Description: 
+ */
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 fn main() {
     cf2app_lib::run()
 }
